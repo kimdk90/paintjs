f76baab205a914e2848059c3233b98082106b460
kimdk90 <55478670+kimdk90@users.noreply.github.com>
2022-02-17 17:45:17 +0900

    2022/02/17 jsCanvas

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # paintjs
 Painting Board made with VanillaJs
+
+canvas를 이용한 그림판 만들기
---
+++ app.js
@@ -0,0 +1,51 @@
+const canvas = document.getElementById("jsCanvas");
+const colors = document.getElementsByClassName("controls_color");
+const ctx = canvas.getContext("2d");
+
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting(e) {
+    painting = false;
+}
+
+function startPainting(e) {
+    painting = true;
+}
+
+function onMouseMove(e) {
+    const X = e.offsetX;
+    const Y = e.offsetY;
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(X,Y);
+    } else {
+        ctx.lineTo(X,Y);
+        ctx.stroke();
+        //ctx.closePath();
+    }
+}
+
+function onMouseDown(e) {
+    painting = true;
+}
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
+
+
+function handleColorClick(e) {
+    const selColor = e.target.style.backgroundColor;
+    ctx.strokeStyle = selColor;
+};
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
